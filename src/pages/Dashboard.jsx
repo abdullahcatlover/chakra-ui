3a0a6f8f60aa5013e1ecd621d8abf3eed9af07f8
cyber-abdullahh <abdullahabumariyam@gmail.com>
2023-07-13 00:49:51 +0500

    1

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Dashboard = () => {
+  return (
+    <div>
+        Dashboard
+       
+    </div>
+  )
+}
+
+export default Dashboard
